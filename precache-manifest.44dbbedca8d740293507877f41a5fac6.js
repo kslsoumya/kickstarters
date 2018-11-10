46f385171d4d48ef7d27e852842fab5f49d39c98
Soumya Konduri <kslsoumya@gmail.com>
2018-11-10 23:00:51 +0530

    Updates

---
+++ precache-manifest.44dbbedca8d740293507877f41a5fac6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "d87395182ea9646a7cf0",
+    "url": "/kickstarters/static/js/runtime~main.d8739518.js"
+  },
+  {
+    "revision": "15a95eb085fd1203a413",
+    "url": "/kickstarters/static/js/main.15a95eb0.chunk.js"
+  },
+  {
+    "revision": "02a3971730b588705231",
+    "url": "/kickstarters/static/js/1.02a39717.chunk.js"
+  },
+  {
+    "revision": "a314acddc822e7630d59ef7e8ad9a9bc",
+    "url": "/kickstarters/index.html"
+  }
+];
